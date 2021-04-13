02717da907d14bd22528a089b02c4ac27c18774d
tanya0311 <tanya03.11@mail.ru>
2021-04-13 18:13:43 +0300

    add menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu'
+// https://material-ui.com/ru/getting-started/installation/
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <AppBar position="fixed">
+      <Container fixed>
+      {/* Toolbar //компонента куда вклавдывают иконки */}
+        <Toolbar>  
+          <IconButton edge='start' color="inherit" aria-label='menu'>
+            <MenuIcon/>
+            <Typography variant="h5" >Web blog</Typography>
+             {/* Typography компонента где прописываем блог */}
+             {/* Box компонента типа div mr={3}-margin */ }
+            <Box mr={3}>
+        <Button color='inherit' variant='outlined'>log in</Button>
+            </Box>
+            <Button color='secondary' variant='contained'>sing up</Button>
+
+          </IconButton>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
 
